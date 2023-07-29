cdb6a6a58fb437e627b152a677933cd7a1ece2bb
André Ferreira <hello@andreferreira.me>
2023-07-29 09:52:56 +0100

    Hello LLMs and ChatMessages

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import os
 from dotenv import load_dotenv
 from langchain.llms import OpenAI
+from langchain.chat_models import ChatOpenAI
 
 load_dotenv()
 
 OPENAI_API_KEY=os.environ.get("OPENAI_API_KEY")
 
 llm = OpenAI(openai_api_key=OPENAI_API_KEY)
+chat_model = ChatOpenAI()
 
-print(llm)
+print(llm.predict("Hi!"))
+print(chat_model.predict("Hi!"))
